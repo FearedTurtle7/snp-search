35e7725288792dcd1b44fb34d30a078556fa540d
logan00 <logan@cookfamily.email>
2020-07-05 13:50:43 -0500

    added resume and fixed cores/step switchup

--- run.py
+++ run.py
@@ -6,28 +6,32 @@ def main(argv):
     outputfile = ''
     step_amount = 0
     cores = 0
+    resume = False
     
     try:
-        opts, args = getopt.getopt(argv, "hi:o:s:c:")
+        opts, args = getopt.getopt(argv, "hi:o:s:c:r")
         print(opts)
+        print(args)
     except getopt.GetoptError:
-        print("run.py -i <inputfile> -o <outputfile> -s <checkpoint length> (optional) -c <cores> (optional)")
+        print("run.py -i <inputfile> -o <outputfile> -s <checkpoint length> -c <cores>")
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
-            print("run.py -i <inputfile> -o <outputfile> -s <step amount> (optional) -c <cores> (optional)")
+            print("run.py -i <inputfile> -o <outputfile> -s <step amount> -c <cores>")
             sys.exit()
         elif opt == "-i":
             inputfile = arg
         elif opt == "-o":
             outputfile = arg
-        elif opt == "-s":
-            cores= int(arg)
         elif opt == "-c":
+            cores= int(arg)
+        elif opt == "-s":
             step_amount = int(arg)
+        elif opt == "-r":
+            resume = True
         
     
-    perform_snp_search(inputfile, outputfile, step_amount, cores)
+    perform_snp_search(inputfile, outputfile, step_amount, cores, resume)
         
         
 if __name__ == "__main__":
